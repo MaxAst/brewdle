2cdaf00fbcec9ce73113c5dbf762b53d2c59b5c1
Max Ast <maxast31@gmail.com>
2021-07-22 16:59:37 +0200

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "pastel-violet": "#E9DEFA",
+        "pastel-yellow": "#FBFCDB",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
